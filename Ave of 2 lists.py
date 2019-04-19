4d1582925612ed3077b8f3154d2eb92cb2bc0b8e
negaryuki <negar.norouzi94@gmail.com>
2019-04-19 15:53:05 +0330

    modified average modified total modified list name = right_list

--- Ave of 2 lists.py
+++ Ave of 2 lists.py
@@ -1,11 +1,12 @@
 num_List = [9, 23, 1, 2, 3, 4, 5, 6, 7, 8, 77, 9, 10, 11]
-a_list = num_List[:8]
+right_list = num_List[:8]
+lenght_lists= len(num_List)//2
 
-ave_right = max(a_list) / len(num_List)
+ave_right = max(right_list) / lenght_lists
 
 left_list = num_List[8:]
 
-ave_left = max(left_list) / len(num_List)
+ave_left = max(left_list) / lenght_lists
 
 total = ave_left - ave_right
 
