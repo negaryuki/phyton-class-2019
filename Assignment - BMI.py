5a654a3860bf7cbdab57ab3bdbc255532c75e095
negarYuki <negar_norouzi94@gmail.com>
2019-04-21 15:43:58 +0430

    Finished ! Final Edition ! \(^o^)/ Date: 21. April 2019

--- Assignment - BMI.py
+++ Assignment - BMI.py
@@ -6,19 +6,18 @@ print('Almost there, now please enter your height(m):')
 height = float(input())
 
 BMI = weight / (height * height)
-
+print('and your BMI is: ', BMI)
 
 if BMI <= 18.5:
-    print('your BMI is: ', BMI)
     print('Result is : Underweight :(')
 elif 18.5 < BMI <= 25:
-    print('your BMI is: ', BMI)
-    print('Result is : Normal :)')
+
+    print('Result is : Normal :) Great !')
 elif 25 < BMI < 30:
-    print('your BMI is: ', BMI)
+
     print('Result is : Overweight :(')
 else:
-    print('your BMI is: ', BMI)
+
     print('Result is : Obesity :((')
 
 
