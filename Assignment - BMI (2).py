e5c5445a8e469458d9852b72670776c5ca7b7f88
negarYuki <negar_norouzi94@gmail.com>
2019-04-22 17:57:24 +0430

    Finished ! Final Edition ! \(^o^)/ Date: 21. April 2019

---
+++ Assignment - BMI (2).py
@@ -0,0 +1,26 @@
+print("Welcome to Negar's BMI calculator ! :D ")
+
+print('Please enter your weight(Kg):')
+weight = float(input())
+print('Almost there, now please enter your height(m):')
+height = float(input())
+BMI = weight / (height * height)
+
+print('Are the inputs based on SI?')
+    if weight.is
+
+print('and your BMI is: ', BMI)
+
+if BMI <= 18.5:
+    print('Result is : Underweight :(')
+elif 18.5 < BMI <= 25:
+
+    print('Result is : Normal :) Great !')
+elif 25 < BMI < 30:
+
+    print('Result is : Overweight :(')
+else:
+
+    print('Result is : Obesity :((')
+
+
