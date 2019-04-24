84c6196bad320990e836a989eac65f0505fdb882
negaryuki <negar.norouzi94@gmail.com>
2019-04-24 20:23:24 +0430

    trying to solve the arrow problem. let's give it some style

--- Assignment - Mirror.py
+++ Assignment - Mirror.py
@@ -1,15 +1,12 @@
+print(' Imagine a Mirror, right in the middle of the below list: ')
 text = "Left -> -> -> -> Right -> -> -> "
-
 mid = len(text) // 2
-
 right = text[mid:]
 left = text[:mid]
-
+print(right, left)
+print("now let's see what this mirror does to the list:")
 mirror_right = right[::-1]
 mirror_left = left[::-1]
 
-print(right, left)
-
-print(mirror_right, right)
-print(left, mirror_left)
-
+print('right side : ', mirror_right, right)
+print('left side : ', left, mirror_left)
