8bbf55cf894391605bb90d3eef3b38c9ff70b6d9
negaryuki <negar.norouzi94@gmail.com>
2019-05-10 15:52:40 +0430

    N/A

---
+++ class_work/S5/sets_in python.py
@@ -0,0 +1,15 @@
+my_set = {1, 2, 3, 1, 1, 1, 1, 1}
+
+print(my_set)
+
+list_of_numbers = list(my_set)
+print(list_of_numbers)
+
+lst = [1, 2, 3, 3, 3, 3, 4, 4, 4, ]
+
+print(set(lst))
+
+
+lst = list(my_set)
+
+print(lst)
