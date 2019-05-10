7eccf4ad373f8b12f58340d179920c56eebe901b
negaryuki <negar.norouzi94@gmail.com>
2019-05-10 18:51:24 +0430

    turtle !!

---
+++ class_work/S5/tamrin.py
@@ -0,0 +1,12 @@
+lst = [0.0, 1.5, 2.2, 2.8, 3.1, 9.2]
+start = lst[:-1]
+end = lst[1:]
+
+lst_sorted =[]
+
+for i in range(len(lst) - 1):
+    print(end[i] - start[i])
+    difference = end[i] - start[i]
+    difference.append[lst_sorted]
+
+    print(lst_sorted)
