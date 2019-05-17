71f932732e057fdc600df8f7d12f34e4131c0430
negarYuki <negar.norouzi94@gmail.com>
2019-05-17 17:26:20 +0430

    find the word2

--- class_work/s6/find_word.py
+++ class_work/s6/find_word.py
@@ -1,5 +1,8 @@
 word = "been"
-text = "half a bee, always a honeybee"
+text = "half a bee, always a bee"
 
-res= text.find("bee")
+res = text.find("bee")
+
+res_2 = text.find("bee", res + 2)
 print(res)
+print(res_2)
