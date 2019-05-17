9e77e41684dcba63cdf068d80c52fe8db04cbc73
negarYuki <negar.norouzi94@gmail.com>
2019-05-17 17:32:23 +0430

    find the word with function

--- class_work/s6/find_word.py
+++ class_work/s6/find_word.py
@@ -1,8 +1,22 @@
-word = "been"
+word = "bee"
 text = "half a bee, always a bee"
 
-res = text.find("bee")
 
-res_2 = text.find("bee", res + 2)
-print(res)
-print(res_2)
+##res = text.find("bee")
+##res_2 = text.find("bee", res + 2)
+# print(res)
+# print(res_2)
+
+def find_all(text, word):
+    ind = -1
+    out = []
+    while True:
+        ind = text.find(word, ind + 1)
+        if ind == -1:
+            break
+        out.append(ind)
+    return out
+
+
+finder = find_all(text, word)
+print(finder)
