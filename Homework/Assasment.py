93949793f8d507363bb36a186bfaed8267ce68fd
negarYuki <negar.norouzi94@gmail.com>
2019-06-03 21:04:09 +0430

    working on it

--- Homework/Assasment.py
+++ Homework/Assasment.py
@@ -1,6 +1,7 @@
 data = [50.3, 338.4, 626.6, 959.4, 1317.9, 1716.7, 2134.3, 2565.5, 3085.6, 3626.7]
 
-#Part 1
+
+# Part 1
 
 def duration(input_lst):
     i = 0
@@ -15,3 +16,24 @@ def duration(input_lst):
 duration_of_lst = duration(data)
 print(duration_of_lst)
 
+
+# Part 2
+
+def duration_description(input_lst):
+    t1 = 300.0
+    t2 = 400.0
+    i = 0
+    while i + 1 == len(input_lst):
+        if duration(input_lst) < t1:
+            desc_duration = "S"
+        elif duration(input_lst) >= t2:
+            desc_duration = "L"
+        else:
+            desc_duration = "M"
+        i += 1
+        duration(input_lst).pop(0)
+        return desc_duration
+
+
+desc = duration_description(data)
+print(desc)
