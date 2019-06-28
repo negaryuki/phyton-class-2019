e04a7788f48f67cae16afc63ea058f292c57a99a
negarYuki <negar.norouzi94@gmail.com>
2019-06-28 17:21:15 +0430

    Ok got it, the last print(prime_or_not) was unnecessary 　今は大丈夫よ じゃ～～次！

---
+++ Exercises/List_ends_function.py
@@ -0,0 +1,19 @@
+# Exercise 12
+# Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25])
+# and makes a new list of only the first and last elements of the given list.
+# For practice, write this code inside a function.
+
+
+a = [5, 10, 15, 20, 25]
+
+new_lst = []
+
+
+# No 1
+def list_maker(a_list):
+    return [a_list[0], a_list[len(a_list) - 1]]
+
+
+newlst = list_maker(a)
+
+print(newlst)
