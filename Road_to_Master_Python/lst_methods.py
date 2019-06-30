e057994051ff3a4ed4756ebea4b159c6eaed2a14
negarYuki <negar.norouzi94@gmail.com>
2019-06-30 19:04:21 +0430

    the difference between .append() and .extend()

--- Road_to_Master_Python/lst_methods.py
+++ Road_to_Master_Python/lst_methods.py
@@ -19,3 +19,21 @@ lst3 = lst2.copy()
 print(lst3)
 
 # _________________________________________________________________________
+# Method4: count()
+
+lst3.append('a')
+print(lst3)
+print(lst3.count('a'))
+# _________________________________________________________________________
+# Method5: extend()
+
+lst3.extend([1, 2])
+print(lst3)
+lst3.append([1, 2])
+print(lst3)
+#IMPORTANT POINT: The Difference between .append and . extend is that if you use :
+# .append for 2 data, the data will be nested in the list : ['a', [1, 2]]
+#.extend for 2 data, then the data will be added just normally to the list : ['a', 1, 2]
+
+# _________________________________________________________________________
+# Method6:
