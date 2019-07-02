fcafae2f4e6c3e5df2255a6df4257624dccdaeb3
negarYuki <negar.norouzi94@gmail.com>
2019-07-02 22:51:44 +0430

    Done with Tuple Methods

--- Road_to_Master_Python/Tuple_Methods.py
+++ Road_to_Master_Python/Tuple_Methods.py
@@ -5,3 +5,12 @@ tpl = (1, 2, 3, 4, 1, 3, 3, 3, 1)
 print(tpl)
 print(tpl.count(1))
 print(tpl.count(3))
+
+# _________________________________________________________________________
+# Method2: index()
+
+print(tpl)
+print(tpl.index(2))
+print(tpl.index(3))
+
+# in this case, .index() will only define the first time '3' appeared in the tuple.
